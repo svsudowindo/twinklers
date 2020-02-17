947ca47e7a134b0816f511aa4263ba9c48981050
nag <nag243@gmail.com>
2020-02-18 01:24:44 +0530

    required components created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -15,7 +15,24 @@ const routes: Routes = [
   {
     path: 'products',
     loadChildren: () => import('./views/products/products.module').then(m => m.ProductsModule)
-  }
+  },
+  {
+    path:'my-account/login',
+    loadChildren:()=>import('./views/login/login.module').then(m =>m.LoginModule)
+  },
+  {
+    path:'my-account/registration',
+    loadChildren:()=>import('./views/registration/registration.module').then(m=>m.RegistrationModule)
+  },
+  {
+    path:'my-account/forgotpassword',
+    loadChildren:()=>import('./views/forgot-password/forgot-password.module').then(m => m.ForgotPasswordModule)
+  },
+  {
+    path:'**',
+    loadChildren:() => import('./views/page-not-found/page-not-found.module').then (m =>m.PageNotFoundModule)
+  },
+
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared/shared.module';
 import { TopbarComponent } from './shared/shared/components/topbar/topbar.component';
+import { LoginComponent } from './views/login/login.component';
+import { ForgotPasswordComponent } from './views/forgot-password/forgot-password.component';
+import { PageNotFoundComponent } from './views/page-not-found/page-not-found.component';
+import{RegistrationComponent} from './views/registration/registration.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopbarComponent
+    TopbarComponent,
+    LoginComponent,
+    ForgotPasswordComponent,
+    PageNotFoundComponent,
+    RegistrationComponent
   ],
   imports: [
     BrowserModule,
