c806c5f14738858bd5e6e31447e512d78cf85ecb
nag <nag243@gmail.com>
2020-02-21 19:38:57 +0530

    carousel custom images implemented.

--- src/app/views/user-views/dashboard/dashboard.component.ts
+++ src/app/views/user-views/dashboard/dashboard.component.ts
@@ -6,7 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
-  images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/1366/560`);
+
+
+  images = [
+    { img: 'assets/images/Carousel/c-1.jpg', label: 'sample1' },
+    { img: 'assets/images/Carousel/c-2.jpg', label: 'sample2' },
+    { img: 'assets/images/Carousel/c-3.jpg', label: 'sample3' }
+  ]
 
   constructor() { }
 
