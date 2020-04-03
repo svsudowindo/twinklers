497a27428dae1fb76ec57327d9b9bac74ca3c669
sai korthivada <sai.korthivada@xcubelabs.com>
2020-04-03 13:22:03 +0530

    Implemented countries, states, cities API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'twinklers';
+  constructor(private httpClient: HttpClient){
+    let headers: HttpHeaders = new HttpHeaders();
+    headers = headers.append('Accept', 'application/json');
+    headers = headers.append('api-token', 'wRgD_XjN8begt0Vfn-Py1qvbc-40ppzWROzxHlKHUOYtTRBz6Dk1kHemObNkiaNG9WU');
+    headers = headers.append('user-email', 'saichoclate68@gmail.com');
+    this.httpClient.get('https://www.universal-tutorial.com/api/getaccesstoken', {headers}).subscribe(res => {
+      console.log(res['auth_token']);
+      let authTokenHeaders: HttpHeaders =  new HttpHeaders();
+      authTokenHeaders = authTokenHeaders.append('Authorization', 'Bearer ' + res['auth_token']);
+      authTokenHeaders = authTokenHeaders.append('Accept', 'application/json');
+      // for countries = https://www.universal-tutorial.com/api/countries
+      // for states =   https://www.universal-tutorial.com/api/states/{name}
+      // for cities = https://www.universal-tutorial.com/api/cities/Alaska
+      this.httpClient.get('https://www.universal-tutorial.com/api/cities/Telangana', {headers: authTokenHeaders}).subscribe(countries => {
+        console.log(countries);
+      });
+    });
+  }
 }
