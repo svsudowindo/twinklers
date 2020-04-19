79d4bb1a733c5285c8ec06824a2eaff0a53d8967
sai korthivada <sai.korthivada@xcubelabs.com>
2020-04-19 21:01:00 +0530

    Error handling on file upload into node js

--- src/api/controllers/common/image-uploader/imageUploader.dao.js
+++ src/api/controllers/common/image-uploader/imageUploader.dao.js
@@ -4,16 +4,17 @@ var fs = require('fs');
 var path = require('path');
 exports.uploadImageAndGetURL = (req, res, next) => {
 
-    var tmp_path = req.file.path;
+  const file = req.file;
+  const match = ["image/png", "image/jpeg"];
+  if (match.indexOf(file.mimetype) === -1) {
+    return res.send(Utils.sendResponse(500, null, ['Please upload either png or jpeg Images'], 'Please upload either png or jpeg Images'));
+  }
+    var tmp_path = file.path;
     const splitForExtention = req.file.originalname.split('.')
     const ext = splitForExtention[splitForExtention.length - 1];
-    /** The original name of the uploaded file
-        stored in the variable "originalname". **/
       const fileName = req.file.filename + (new Date().getTime()).toString() + '.' + ext
-    // var target_path = __dirname + 'public/uploads/' + fileName;
     var target_path = path.join(process.cwd(), 'public/uploads/', fileName)
   
-    /** A better way to copy the uploaded file. **/
     var src = fs.createReadStream(tmp_path);
     var dest = fs.createWriteStream(target_path);
     src.pipe(dest);
