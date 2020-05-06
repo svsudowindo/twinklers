ef5a924cc9bef7fe7a56fbb151dc292ec90a4e59
sai korthivada <sai.korthivada@xcubelabs.com>
2020-05-06 09:04:12 +0530

    Changed Get categories and Get Product by id into common

--- src/api/routes/admin-routes.js
+++ src/api/routes/admin-routes.js
@@ -2,19 +2,16 @@ var express = require('express');
 var adminRoutes = express.Router();
 var CategoryDAO = require('../controllers/admin/category/category.dao');
 var UserDAO = require('../controllers/auth/user/user.dao');
-var ProductDAO= require('../controllers/admin/products/products.dao');
+var ProductDAO = require('../controllers/admin/products/products.dao');
 
 
 adminRoutes.post('/modify-category', CategoryDAO.createCategory);
 
-adminRoutes.get('/get-categories', CategoryDAO.getAllCategories);
-
-adminRoutes.get('/get-all-users/:id',UserDAO.getAllUsers);
+adminRoutes.get('/get-all-users/:id', UserDAO.getAllUsers);
 
 adminRoutes.post('/create-product/:id', ProductDAO.createProduct);
 
 adminRoutes.get('/get-all-products-by-status', ProductDAO.getAllProductsByAdmin);
 
-adminRoutes.get('/get-product-by-id/:productID', ProductDAO.getProductDetailsByAdminByID);
 
 exports.adminRoutes = adminRoutes;
