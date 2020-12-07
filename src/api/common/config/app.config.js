cf583a8f9d0d0d6e3cfdc93358caf2f72d1a717f
Sai <sai@PurpleTalks-MacBook-Air.local>
2020-12-07 23:12:44 +0530

    Removed node js code and moved code into twinklers api repo

--- src/api/common/config/app.config.js
+++
@@ -1,6 +0,0 @@
-APP_CONFIG =  {
-  PORT: 4500,
-  IMAGE_RESPONSE_URL: 'http://localhost:4500/'
-}
-
-module.exports = APP_CONFIG;
--- src/api/routes/user-routes.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var userRoutes = express.Router();
-var CategoryDAO = require('../controllers/admin/category/category.dao');
-var UserDAO = require('../controllers/auth/user/user.dao');
-var ProductDAO= require('../controllers/admin/products/products.dao');
-
-
-exports.userRoutes = userRoutes;
